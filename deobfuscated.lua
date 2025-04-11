c9c76ecd373df58c2055c0fe2c475b9e7273821c
kobot <164139791+AnhEmTu@users.noreply.github.com>
2025-04-11 20:58:03 +0700

    Update deobfuscated.lua

--- deobfuscated.lua
+++ deobfuscated.lua
@@ -1,5 +1,5 @@
 
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/AnhEmTu/Webhook-discord/refs/heads/main/T"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/AnhEmTu/Webhook-discord/refs/heads/main/Notification.lua"))()
 
 repeat wait() until game.Players
 repeat wait() until game.Players.LocalPlayer
